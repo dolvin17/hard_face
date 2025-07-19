6610a238e2b3e23299a3dce6e064c954d128a7a5
dolvin17 <huertas876@gmail.com>
2025-07-20 00:04:37 +0200

    feat:hooks updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client"
 
+import Navigation from "@/components/Navigation";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 
 export default function Home() {
   return (
  <>
+ <Navigation />
  <ConnectButton />
  </>
   );
